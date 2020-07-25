9b481a53f22e1169652eddd8b4a3e8f5e00c42e1
Kuchi Sri Bhargav Ram <sbramkuchi@gmail.com>
2020-07-25 22:22:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import image from './image.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      color: "red"
+    }
+  }
+  render() {
+    return (
+      <div className='body'>
+        <h1>Farewell 2020</h1>
+        <img src={image} alt="image" width="600px" height="400px" />
+      </div>
+    );
+  }
 }
+
 
 export default App;
