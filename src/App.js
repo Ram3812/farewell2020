70d0b64b89174e7b3549aaffd706b8a60f210f3b
Kuchi Sri Bhargav Ram <sbramkuchi@gmail.com>
2025-09-03 18:24:55 +0530

    refactor: rename project to solar-demo and update dependencies
    - Changed project name from "farewell" to "solar-demo" and updated version to 1.0.0 in package.json.
    - Removed unused HTML files (Farewell2020.html, Farewell2020phn.html) and favicon.ico.
    - Updated index.html to reflect new title and removed unnecessary meta tags.
    - Deleted manifest.json and robots.txt as they are no longer needed.
    - Removed App.css and index.css as styles were not used.
    - Refactored App.js to a functional component and added a navigation bar with links.
    - Removed serviceWorker.js and setupTests.js as they are not required.
    - Deleted unused images and added a new logo image.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
-import React from 'react';
-import './App.css';
-import lapimage from './lapimage.jpg';
+import React from "react";
+import logo from "./images/logo.jpg"; // Import logo
 
-const url = "https://meet.google.com/cck-zfsk-aou";
+const navStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  padding: "0.5rem 1rem",
+  background: "#222",
+  color: "#fff",
+  flexWrap: "wrap"
+};
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      color: "red"
-    }
-  }
-  render() {
-    return (
-      <div>
-        <div className='container'>
-          <div className='content'>
-            <center>
-              <p>
-                We are waiting<br />
-                <a href={url}>Join us here!</a>
-              </p>
-            </center>
-          </div>
+const logoStyle = {
+  height: "40px",
+  cursor: "pointer"
+};
+
+const navLinksStyle = {
+  display: "flex",
+  gap: "1rem"
+};
+
+const linkStyle = {
+  color: "#fff",
+  textDecoration: "none",
+  fontSize: "1rem"
+};
+
+function App() {
+  return (
+    <div>
+      <nav style={navStyle}>
+        <a href="/" style={{ display: "flex", alignItems: "center" }}>
+          <img src={logo} alt="Logo" style={logoStyle} />
+        </a>
+        <div style={navLinksStyle}>
+          <a href="#about" style={linkStyle}>About Us</a>
+          <a href="#contact" style={linkStyle}>Contact Us</a>
         </div>
-      </div>
-    );
-  }
+      </nav>
+      <main style={{ padding: "1rem" }}>
+        <h1>Hello, React!</h1>
+        <p>This is a basic React app template.</p>
+      </main>
+    </div>
+  );
 }
-
 
 export default App;
